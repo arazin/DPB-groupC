a4045cc3e76ea5aee65392598a6f10e4f247bb2e
arazin <mailaddress_20120316@me.com>
2013-11-27 17:22:38 +0900

    ユーザー情報一覧を改めて作成

--- app/Controller/UsersController.php
+++ app/Controller/UsersController.php
@@ -5,22 +5,25 @@ class UsersController extends AppController {
 	public function beforeFilter() {
     parent::beforeFilter();
 		/* $this->Auth->allow('add'); // ユーザーに自身で登録させる */
-}
+	}
 	
+	public function index(){
+		$this->set('users',$this->User->find('all'));
+	}
 	
 	public function login() {
     if ($this->request->is('post')) {
-        if ($this->Auth->login()) {
-            $this->redirect($this->Auth->redirect());
-        } else {
-            $this->Session->setFlash(__('Invalid username or password, try again'));
-        }
+      if ($this->Auth->login()) {
+        $this->redirect($this->Auth->redirect());
+      } else {
+        $this->Session->setFlash(__('Invalid username or password, try again'));
+      }
     }
-}
+	}
 
-public function logout() {
+	public function logout() {
     $this->redirect($this->Auth->logout());
-}
+	}
 	
 }
 
