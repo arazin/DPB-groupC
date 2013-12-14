34102241512a739f7974575302c87e33e7e34abe
arazin <mailaddress_20120316@me.com>
2013-12-14 11:45:32 +0900

    part/addの完成。uapp認証機構途中

--- app/Controller/ParticipantsController.php
+++ app/Controller/ParticipantsController.php
@@ -1,7 +1,59 @@
 <?php
 class ParticipantsController extends AppController{
 	public $scaffold;
+	/*
+	 * public $component
+	 * Security:悪質なリクエストを防ぐ Paginator:便利な一覧表示
+	 */
+	public $components = array('Security','Paginator');
+	
+	//paginatorのoption
+	public $paginate=array(
+		'contain' => array(''),
+		'condition' => array('Group.group_name'=>'unapprove'),
+		'limit' => 25,
+		'order' => array('User.created'=>'asc'),
+		'recursive' => '2',
+	);
+	
+	/*
+	 * アクター:大学 未認証の参加者一覧
+	 */
+	public function uapplist(){
+		$this->Paginator->settings = $this->paginate;
+		$this->set('data',$this->Paginator->paginate());
+	}
 
+	
+	/*
+	 * アクター:大学 未認証の参加者を認証
+	 */
+	public function approve($id=NULL){
+		$this->request->onlyAllow('post');
+		
+    $this->Participant->User->id = $id;
+    if (!$this->Participant->User->exists()) {
+      throw new NotFoundException(__('Invalid user'));
+    }
+		/*
+		 * 参加者のグループidをセット
+		 */
+		$findoption = array(
+			'conditions' => array('Group.name'=>'participants'),
+			'fields' => array('id'),
+			'recursive' => 0,
+		);
+		$tmp=$this->Participant->User->Group->find('first',$findoption);
+		$this->request->data['User']['group_id']=$tmp['Group']['id'];
+		if($this->Participant->User->save($this->request->data)){
+			$this->Session->setFlash(__('参加者が認証されました'));
+			$this->redirect(array('action'=>'uapplist'));
+		}
+	}
+	
+	/*
+	 * アクター:誰でも 未認証として登録
+	 */
 	public function add(){
 		//業種
 		$findoption = array(
@@ -24,9 +76,13 @@ class ParticipantsController extends AppController{
 			'recursive' => 0,//関連テーブルからは検索しない
 		);
 		$this->set('courses',$this->Participant->Course->find('list',$findoption));
+
+		/*
+		 * 登録ロジック
+		 */
 		if($this->request->is('post')){
 			/*
-			 * 参加者のグループidをセット
+			 * 参加者(未認証)のグループidをセット
 			 */
 			$findoption = array(
 				'conditions' => array('Group.name'=>'unapprove'),
@@ -37,7 +93,7 @@ class ParticipantsController extends AppController{
 			$this->request->data['User']['group_id']=$tmp['Group']['id'];
 
 			/*
-			 *登録ロジック
+			 * 登録
 			 */
 			$this->Participant->User->create();
 			//まず値の正当性チェックのみ
