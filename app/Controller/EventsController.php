c6da659e15ac4bb381c81f8fa60c883c9fc90d4e
MYAX <myaxsan@gmail.com>
2013-12-18 11:48:14 +0900

    イベントページとトップの+アルファ
    変更

--- app/Controller/EventsController.php
+++ app/Controller/EventsController.php
@@ -1,6 +1,23 @@
-<?php
+﻿<?php
 class EventsController extends AppController{
-	public $scaffold;
+	/*public $scaffold;*/
+ public $helpers = array('Html', 'Form');
+
+    public function events() {
+         $this->set('events', $this->Event->find('all'));
+    }
+
+    public function view($id = null) {
+        if (!$id) {
+            throw new NotFoundException(__('Invalid event'));
+        }
+
+        $event = $this->Event->findById($id);
+        if (!$event) {
+            throw new NotFoundException(__('Invalid event'));
+        }
+        $this->set('event', $event);
+    }
 }
 
 ?>
